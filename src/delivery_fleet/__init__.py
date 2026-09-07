a801f3377d26a9aac134bde34ab56e3370fcdf9b
Noam Avital <noam.avital@campus.technion.ac.il>
2026-09-07 14:22:30 +0300

    Export robot routing and benchmark policy APIs

--- src/delivery_fleet/__init__.py
+++ src/delivery_fleet/__init__.py
@@ -1,14 +1,27 @@
+from .battery_routing import (
+    BatteryFeasibleRoute,
+    BatteryFeasibleRouter,
+    ChargeEvent,
+    NoFeasibleBatteryRoute,
+    RouteSegment,
+)
 from .charging import (
+    DISTANCE_TO_NEAREST_CHARGING_STATION_M_ATTR,
+    NEAREST_CHARGING_STATION_NODE_ATTR,
     ChargingConfig,
     ChargingStation,
     add_charging_stations,
+    annotate_nearest_charging_stations,
     max_distance_to_nearest_station,
+    nearest_station_data,
     select_charging_station_nodes,
 )
 from .defaults import (
     MAX_DISTANCE_TO_CHARGING_STATION_M,
     MIN_ROBOT_FULL_BATTERY_RANGE_M,
 )
+from .policies import Assignment, NearestAvailableRobotPolicy
+from .robot import RobotSpec, RobotState
 from .scenario_creator import (
     ImportanceDistribution,
     Item,
@@ -21,19 +34,32 @@ from .scenario_creator import (
 )
 
 __all__ = [
+    "Assignment",
+    "BatteryFeasibleRoute",
+    "BatteryFeasibleRouter",
+    "ChargeEvent",
     "ChargingConfig",
     "ChargingStation",
+    "DISTANCE_TO_NEAREST_CHARGING_STATION_M_ATTR",
     "ImportanceDistribution",
     "Item",
     "ItemDistribution",
     "MAX_DISTANCE_TO_CHARGING_STATION_M",
     "MIN_ROBOT_FULL_BATTERY_RANGE_M",
+    "NEAREST_CHARGING_STATION_NODE_ATTR",
+    "NearestAvailableRobotPolicy",
+    "NoFeasibleBatteryRoute",
     "NodeDemandProfile",
     "Order",
+    "RobotSpec",
+    "RobotState",
+    "RouteSegment",
     "Scenario",
     "ScenarioCreator",
     "add_charging_stations",
+    "annotate_nearest_charging_stations",
     "constant_demand_profile",
     "max_distance_to_nearest_station",
+    "nearest_station_data",
     "select_charging_station_nodes",
 ]
