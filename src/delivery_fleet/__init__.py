c147f5b9fb93cf2b0391900f6c1b407ce55b8bbe
Noam Avital <noam.avital@campus.technion.ac.il>
2026-09-07 13:55:56 +0300

    Export charging coverage defaults

--- src/delivery_fleet/__init__.py
+++ src/delivery_fleet/__init__.py
@@ -2,7 +2,12 @@ from .charging import (
     ChargingConfig,
     ChargingStation,
     add_charging_stations,
+    max_distance_to_nearest_station,
     select_charging_station_nodes,
+)
+from .defaults import (
+    MAX_DISTANCE_TO_CHARGING_STATION_M,
+    MIN_ROBOT_FULL_BATTERY_RANGE_M,
 )
 from .scenario_creator import (
     ImportanceDistribution,
@@ -21,11 +26,14 @@ __all__ = [
     "ImportanceDistribution",
     "Item",
     "ItemDistribution",
+    "MAX_DISTANCE_TO_CHARGING_STATION_M",
+    "MIN_ROBOT_FULL_BATTERY_RANGE_M",
     "NodeDemandProfile",
     "Order",
     "Scenario",
     "ScenarioCreator",
     "add_charging_stations",
     "constant_demand_profile",
+    "max_distance_to_nearest_station",
     "select_charging_station_nodes",
 ]
