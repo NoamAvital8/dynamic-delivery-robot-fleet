993c651b2308ac72d28ab93495201178d0b85f3c
Noam Avital <noam.avital@campus.technion.ac.il>
2026-09-07 13:34:21 +0300

    Export charging station placement API

--- src/delivery_fleet/__init__.py
+++ src/delivery_fleet/__init__.py
@@ -1,3 +1,9 @@
+from .charging import (
+    ChargingConfig,
+    ChargingStation,
+    add_charging_stations,
+    select_charging_station_nodes,
+)
 from .scenario_creator import (
     ImportanceDistribution,
     Item,
@@ -10,6 +16,8 @@ from .scenario_creator import (
 )
 
 __all__ = [
+    "ChargingConfig",
+    "ChargingStation",
     "ImportanceDistribution",
     "Item",
     "ItemDistribution",
@@ -17,5 +25,7 @@ __all__ = [
     "Order",
     "Scenario",
     "ScenarioCreator",
+    "add_charging_stations",
     "constant_demand_profile",
+    "select_charging_station_nodes",
 ]
